ef38e51d67257086432c4193e01142014f1f0356
Vincent Chase <chiwenzhen@gmail.com>
2016-12-19 16:35:05 +0800

    1.update linear_regression_model 2.update logistic_regression_model 3.update test code

--- logitsic_regression.py
+++ logitsic_regression.py
@@ -5,28 +5,10 @@ from descent import gradientdescent
 
 # 逻辑回归
 class LinearRegression:
-    """Class for implimenting Linear Regression"""
-
     def __init__(self):
-        """
-        Attributes::
-            learned (bool): Keeps track of if Linear Regression has been fit
-            weights (np.ndarray): vector of weights for linear regression
-        """
-        self.learned = False
         self.weights = np.NaN
 
     def predict(self, X):
-        """
-        Args:
-            X (np.ndarray): Test data of shape[n_samples, n_features]
-        Returns:
-            np.ndarray: shape[n_samples, 1], predicted values
-        Raises:
-            ValueError if model has not been fit
-        """
-        if not self.learned:
-            raise NameError('Fit model first')
         # Add column of 1s to X for perceptron threshold
         X = np.asarray(X)
         X = np.column_stack((np.ones(np.shape(X)[0]), X))
@@ -34,36 +16,14 @@ class LinearRegression:
         return prediction
 
     def grad(self, X, y, weights):
-        """
-        Computes the gradient (needed if using gradient descent).
-
-        Args:
-            X (np.ndarray): Training data of shape[n_samples, n_features]
-            y (np.ndarray): Target values of shape[n_samples, 1]
-            weights (np.ndarray): Optional use of gradient descent to calculate weights
-                if False, uses closed form solution to calculate weights.
-        Returns:
-            np.array: the gradient of the linear regression cost function
-        """
         hypothesis = np.dot(X, weights) - y
         gradient = np.dot(np.transpose(X), hypothesis) / np.size(y)
         return gradient
 
     def fit(self, X, y, reg_parameter=0):
-        """
-        Currently, only L2 regularization is implemented.
-
-        Args:
-            X (np.ndarray): Training data of shape[n_samples, n_features]
-            y (np.ndarray): Target values of shape[n_samples, 1]
-            reg_parameter (float): float to determine strength of regulatrization  penalty
-                if 0, then no linear regression without regularization is performed
-        Returns: an instance of self
-        """
         y = np.asarray(y)
         X = np.asarray(X)
         X = np.column_stack((np.ones(np.shape(X)[0]), X))
         self.weights = gradientdescent(X, y, self.grad, reg_param=reg_parameter)
-        self.learned = True
         return self
 
--- test/linear_regression_test.py
+++ test/linear_regression_test.py
@@ -1,38 +1,41 @@
 # coding=utf-8
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import datasets
+import random
 from linear_regression import LinearRegression
 
-diabetes = datasets.load_diabetes()
 
-# Use only one feature
-diabetes_X = diabetes.data[:, np.newaxis, 2]
+# 拟合目标函数
+def f(x):
+    return 2 * x + 1
 
-# Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
 
-# Split the targets into training/testing sets
-diabetes_y_train = diabetes.target[:-20]
-diabetes_y_test = diabetes.target[-20:]
+# 生成数据
+def generate(n_samples=100):
+    X = (np.random.rand(n_samples) * 10 - 5)
+    y = f(X) + np.random.normal(0.0, 10, n_samples)
+    return X, y
 
-# Create linear regression object
-regr = LinearRegression()
 
-# Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
+if __name__ == '__main__':
 
-# The mean squared error
-print("Mean squared error: %.2f"
-      % np.mean((regr.predict(diabetes_X_test) - diabetes_y_test) ** 2))
+    # 创建模型
+    regr = LinearRegression()
 
-# Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, regr.predict(diabetes_X_test), color='blue',
-         linewidth=3)
+    # 生成训练和测试数据
+    x_train, y_train = generate(100)
+    x_test, y_test = generate(50)
 
-plt.xticks(())
-plt.yticks(())
+    # 训练模型
+    regr.fit(x_train, y_train)
 
-plt.show()
+    # 测试模型
+    y_pred = regr.predict(x_test)
+
+    # 均方误差
+    print("Mean squared error: %.2f" % (np.mean(y_test - y_test) ** 2))
+
+    # 绘图
+    plt.scatter(x_test, y_pred,  color='red')
+    plt.plot(x_test, f(x_test), color='blue', linewidth=3)
+    plt.show()
